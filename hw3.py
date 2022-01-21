7c4fb85ee93f6a522eab5c04dfd8a3dc47acedc8
Phong Vu <>
2022-01-21 04:21:36 -0500

    finished part 2 and part 3. I also added some citations at the top of the code.

--- hw3.py
+++ hw3.py
@@ -1,3 +1,9 @@
+# https://www.kite.com/python/answers/how-to-make-a-list-into-a-comma-separated-string-in-python
+# https://docs.python.org/3/library/math.html
+# https://stackoverflow.com/questions/35328953/how-to-compare-individual-characters-in-two-strings-in-python-3
+
+import math
+
 def vowels(input): # part 1
 	vowels = 0
 	consonants = 0
@@ -20,4 +26,19 @@ if vowels("elephant") == False: # more consonants than vowels
 	print("False")
 if vowels("elephant") == None: # equal number of vowels and consonants
 	print("None")
+
+def volume(radius, height): # part 2
+	volume = math.pi * height * math.pow(radius, 2) # pi x height x radius^2
+	print("Volume calculated from radius", radius, "and height", height, "equals", volume)
+
+print("The answer to part 2 is: ")
+volume(5, 10)
+
+def csv(strings): # part 3
+	combinedString = ",".join(strings) # joins together the elements in the list of strings and removes the quotations
+	print(combinedString)
+
+print("The answer to part 3 is ")
+listOfStrings = ["pickle", "juice", "is", "sour"]
+csv(listOfStrings)
 
